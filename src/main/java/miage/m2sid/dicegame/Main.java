edd00c25f1f557499b6ee0dbf8ad1a60168569e3
Florian Muller <muller.florian57@gmail.com>
2017-11-11 21:21:21 +0100

    First commit

---
+++ src/main/java/miage/m2sid/dicegame/Main.java
@@ -0,0 +1,24 @@
+package miage.m2sid.dicegame;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/ihm/dice_game.fxml"));
+        primaryStage.setTitle("DiceGame");
+
+        Scene scene = new Scene(root, 647, 359);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
